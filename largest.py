12dd14288a03e1e904229a9efedd68c1341c5158
push114 <34578557+push114@users.noreply.github.com>
2017-12-16 07:06:54 -0800

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,9 @@
+n1=34
+n2=44
+n3=87
+if((n1>=n2)and(n1>n3)):
+	print("the largest number is ",n1)
+elif((n2>=n3)and(n2>=n3)):
+	print("the largest number is ",n2)
+else:
+	print ("the largest numbet is ",n3)
