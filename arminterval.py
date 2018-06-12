827e06501096eb2266c6ea7eec49cc14df1a109f
push114 <34578557+push114@users.noreply.github.com>
2018-06-12 15:01:19 +0530

    Create arminterval.py

---
+++ arminterval.py
@@ -0,0 +1,14 @@
+
+
+x=int(input())
+y=int(input())
+for num in range(x,y+1):
+   order=len(str(num))
+   sum=0
+   temp=num
+   while temp>0:
+	   digit=temp%10
+	   sum+=digit**order
+   	   temp //=10
+   if num==sum:
+	 print(num)
