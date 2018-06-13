eb0a6e1095b2a3fc5d873b4a92193a8d81d31da3
push114 <34578557+push114@users.noreply.github.com>
2018-06-13 11:24:09 +0530

    Create factt.py

---
+++ factt.py
@@ -0,0 +1,10 @@
+n=int(input())
+fact=1
+if n<0:
+     print('factorial does not exist')
+elif n==0:
+     print('factorial of zero is 0')
+else:
+     for i in range (1,n+1):
+          fact=fact*i
+     print(fact)
